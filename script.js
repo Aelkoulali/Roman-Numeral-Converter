c652f77aa538c54efb4c43f4bc1ce6ede4152876
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-04 21:10:22 +0200

    Access to the id from the html document

---
+++ script.js
@@ -0,0 +1,4 @@
+// Access to the id from the html document
+const input = document.getElementById("number");
+const convert = document.getElementById("convert-btn");
+const output = document.getElementById("output");
