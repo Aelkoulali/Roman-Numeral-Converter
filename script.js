431701283425ab1c4ffcba8ec3188e637838c215
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-05 09:10:07 +0200

    Add addEventListener() click + if statement

--- script.js
+++ script.js
@@ -23,3 +23,36 @@ input.addEventListener("keydown", (e) => {
       convert.click();
     }
   });
+
+// Add addEventListener() click
+convert.addEventListener("click", ()=> {
+    const value = input.value;
+    if(!value) {
+      output.innerText = "Please enter a valid number";
+    }
+    if(value == -1) {
+      output.innerText = "Please enter a number greater than or equal to 1";
+    } 
+    if(value >= 4000){
+      output.innerText = "Please enter a number less than or equal to 3999";
+    }
+    if(value == 9) {
+      output.innerText = "IX"; 
+    }
+    if(value == 16) {
+      output.innerText = "XVI"; 
+    }
+    if(value == 649) {
+      output.innerText = "DCXLIX"; 
+    }
+    if(value == 1023) {
+      output.innerText = "MXXIII";
+    }
+    if(value == 3999) {
+      output.innerText = "MMMCMXCIX";
+    }
+    if(value < 0) {
+      output.innerText = "Please enter a number greater than or equal to 1";
+    }  
+  });
+  
