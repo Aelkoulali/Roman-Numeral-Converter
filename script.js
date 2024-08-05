4004f150f8480e93dbb0d0296df742bfd0533279
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-05 09:02:15 +0200

    Add numerals Variable

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 const input = document.getElementById("number");
 const convert = document.getElementById("convert-btn");
 const output = document.getElementById("output");
+const numerals = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["IX", 5],
+    ["IX", 4],
+    ["IX", 1]
+  ];
