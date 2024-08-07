74cf95ce1d496a08e779d611fc839e5d79daaaa6
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-07 11:41:05 +0200

    Optimize code

--- script.js
+++ script.js
@@ -26,23 +26,22 @@ input.addEventListener("keydown", (e) => {
 
 // Add addEventListener() click
 convert.addEventListener("click", ()=> {
-    const value = input.value;
+    let value = input.value;
     if(!value) {
       output.innerText = "Please enter a valid number";
     } else if(value < 0) {
       output.innerText = "Please enter a number greater than or equal to 1";
-    } else if(value >= 4000){
+    } else if(value > 3999){
       output.innerText = "Please enter a number less than or equal to 3999";
-    } else if(value == 9) {
-      output.innerText = "IX"; 
-    } else if(value == 16) {
-      output.innerText = "XVI"; 
-    } else if(value == 649) {
-      output.innerText = "DCXLIX"; 
-    } else if(value == 1023) {
-      output.innerText = "MXXIII";
-    } else if(value == 3999) {
-      output.innerText = "MMMCMXCIX";
+    } else {
+      let result = ""; 
+      for(const[roman,number] of numerals) {
+        while (number < value) {
+          result += roman;
+          value -= number;
+        }
+        output.innerText = result;
+      }     
     } 
   });
   
